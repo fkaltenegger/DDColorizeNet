f4472f0001ed666a10fb0ec486d12ba779caa1d6
Fabian Kaltenegger <fabian.kalti@gmx.at>
2025-04-07 18:06:55 +0200

    Reverted changes made to repo during practical phase

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -173,9 +173,9 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
     def encode_with_transformer(self, text):
         x = self.model.token_embedding(text)  # [batch_size, n_ctx, d_model]
         x = x + self.model.positional_embedding
-        # x = x.permute(1, 0, 2)  # NLD -> LND
+        x = x.permute(1, 0, 2)  # NLD -> LND
         x = self.text_transformer_forward(x, attn_mask=self.model.attn_mask)
-        # x = x.permute(1, 0, 2)  # LND -> NLD
+        x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.model.ln_final(x)
         return x
 
